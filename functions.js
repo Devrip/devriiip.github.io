4122c427ad682d26fae5570071c540068085b8b9
John Jamesson <www.squad@gmail.com>
2019-06-07 05:31:31 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,8 +1,17 @@
 Di.WS.Connect("ws0.dinom.net/8251120b4c0192d35a89105cd2e66b950c2c9f5503ef9ff00290b97793f5b8c6");
 
-Di.$.Pub.Get({
+extendInit=Di.Setup;
+Di.Setup=function(e){
+	extendInit(e);
+	$(document).trigger('loaded');
+};
+
+$(window).on('loaded',function(){
+	Di.$.Pub.Get({
 		'table':'animal_pics'
 	},'RenderPics');
+
+});
 
 
 function RenderPics(r){
